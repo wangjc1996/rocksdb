adbd5a0dd0978efd319f8373d6448c93b36d59cd
Jiachen Wang <wangjc19960926@gmail.com>
2019-05-28 10:21:37 +0800

    State Info keeps info of optimistic & pessimistic count
    optimistic: occ & ic3(dirty)
    pessimistic: 2pl

--- include/rocksdb/utilities/state_info.h
+++ include/rocksdb/utilities/state_info.h
@@ -3,16 +3,16 @@ namespace rocksdb {
 
 static constexpr uint8_t kTotalStates = 4;
 
-static constexpr uint8_t kCleanReadIndex = 0;
-static constexpr uint8_t kDirtyReadIndex = 1;
-static constexpr uint8_t kCleanWriteIndex = 2;
-static constexpr uint8_t kDirtyWriteIndex = 3;
+static constexpr uint8_t kOptimisticReadIndex = 0;
+static constexpr uint8_t kPessimisticReadIndex = 1;
+static constexpr uint8_t kOptimisticWriteIndex = 2;
+static constexpr uint8_t kPessimisticWriteIndex = 3;
 
-template <bool read, bool is_dirty>
+template <bool read, bool is_optimistic>
 static constexpr uint8_t GetStateIndex() {
   return read
-         ? (is_dirty ? kDirtyReadIndex : kCleanReadIndex)
-         : (is_dirty ? kDirtyWriteIndex : kCleanWriteIndex);
+         ? (is_optimistic ? kOptimisticReadIndex : kPessimisticReadIndex)
+         : (is_optimistic ? kOptimisticWriteIndex : kPessimisticWriteIndex);
 }
 
 using StateUnit = uint16_t;
@@ -26,17 +26,17 @@ struct StateInfo {
 
   void SetHandle(StateInfoInternal* h) { handle = h; }
 
-  template <bool read, bool is_dirty>
+  template <bool read, bool is_optimistic>
   inline void IncreaseAccess() {
-    constexpr uint8_t index = GetStateIndex<read, is_dirty>();
+    constexpr uint8_t index = GetStateIndex<read, is_optimistic>();
 #define atomic_inc(P) __sync_add_and_fetch((P), 1)
     atomic_inc(((StateUnit*)handle) + index);
 #undef atomic_inc
   }
 
-  template <bool read, bool is_dirty>
+  template <bool read, bool is_optimistic>
   inline void DecreaseAccess() {
-    constexpr uint8_t index = GetStateIndex<read, is_dirty>();
+    constexpr uint8_t index = GetStateIndex<read, is_optimistic>();
 #define atomic_dec(P) __sync_add_and_fetch((P), -1) 
     atomic_dec(((StateUnit*)handle) + index);
 #undef atomic_dec
